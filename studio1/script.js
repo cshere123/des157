f5271b803475ce69ea5ae39677c04ad320345ce8
seetho-c <cseetho@ucdavis.edu>
2018-01-29 14:30:13 -0800

    Update script.js

--- studio1/script.js
+++ studio1/script.js
@@ -16,7 +16,7 @@ function processForm() {
   var holiday = document.f.holiday.value;
   var numberTwo = document.f.numberTwo.value;
 
-  myMsg.innerHTML = "Dear <em>" + userName + "</em>, <br>You left me s-peach-less since <em>" + classSubject + "</em> class after the professor passed out the <em>" + nounOne + "</em>. <em>" + numberOne + "</em> days have passed and honeydew even know how lucky I am to have met you? I guava lot of things to tell you but I’ll save it for later when we meet. Let’s orange a time to meet on <em>" + holiday + "</em>at <em>" + numberTwo + "</em> o’clock. Save the date!<br><br>Love, <br>Secret admirer";
+  myMsg.innerHTML = "Dear <strong>" + userName + "</strong>, <br>You left me s-peach-less since <strong>" + classSubject + "</strong> class after the professor passed out the <strong>" + nounOne + "</strong>. <strong>" + numberOne + "</strong> days have passed and honeydew even know how lucky I am to have met you? I guava lot of things to tell you but I’ll save it for later when we meet. Let’s orange a time to meet on <strong>" + holiday + "</strong> at <strong>" + numberTwo + "</strong>. Save the date!<br><br>Love, <br>Secret admirer";
 
   return false;
 }
