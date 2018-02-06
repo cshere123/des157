ad38b34e6a1f8520424948d72c87d62b7a7144fb
seetho-c <cseetho@ucdavis.edu>
2018-02-06 01:14:58 -0800

    Update script.js

--- studio2/script.js
+++ studio2/script.js
@@ -2,63 +2,61 @@
 console.log("reading");
 // global variables
 var hover = document.getElementById("hover");
+var bag = document.getElementById("bag");
+
 var tangerine = document.getElementById("tangerine");
 var sharpie = document.getElementById("sharpie");
 var ruler = document.getElementById("ruler");
 var close = document.getElementById("close");
 
-var tangdescrip = document.getElementById("tangdescrip");
-var sharpiedescrip = document.getElementById("ssharpiedescrip");
-var rulerdescrip = document.getElementById("rulerdescrip");
-var overlay = document.getElementById("overlay")
+
+//Does it have to be ID? can it use attribute because i want to use a class
+// var title = document.getElementById("title");
+// var description = document.getElementById("description");
+
 
 //hover over bag to open
 hover.addEventListener("mouseout", function() {
   hover.style.opacity = 1;
   hover.src = "images/backpack.png";
-  close.getElementById = "none";
 });
 hover.addEventListener("mouseover", function() {
   hover.style.opacity = 1;
   hover.style.cursor = "pointer";
   hover.src = "images/hover.png";
-  close.getElementById = "none";
 });
 
-//after clicking on contents
+//items are opened after clicking on the backpack
 hover.addEventListener("click", function() {
-  tangerine.stye.display = "block";
-  hover.style.opacity = 0;
-});
-
-tangerine.addEventListener("mouseover", function() {
-  tangdescrip.style.display = "block";
-  hover.style.cursor = "pointer";
-});
-
-hover.addEventListener("click", function() {
+  hover.style.display = "none";
+  tangerine.style.display = "block";
   sharpie.style.display = "block";
-  hover.style.opacity = 0;
-
+  ruler.style.display = "block";
 });
+
+//items appear, hover over to have descriptions show
+tangerine.addEventListener("mouseover", function() {
+  tangerine.style.cursor = "pointer";
+  title.innerHTML = "Tangerine";
+  description.innerHTML = "Because I need my daily dose of Vitamin C";
+})
 
 sharpie.addEventListener("mouseover", function() {
-  sharpiedescrip.style.display = "block";
-  hover.style.cursor = "pointer";
-});
-
-hover.addEventListener("click", function() {
-  ruler.style.display = "block";
-  hover.style.opacity = 0;
-});
+  sharpie.style.cursor = "pointer";
+  title.innerHTML = "Sharpie";
+  description.innerHTML = "Because line strokes";
+})
 
 ruler.addEventListener("mouseover", function() {
-  rulerdescrip.style.display = "block";
-  hover.style.cursor = "pointer";
-});
+  ruler.style.cursor = "pointer";
+  title.innerHTML = "Mini ruler";
+  description.innerHTML = "Because lines";
+})
 
+//this closes all the items and returns to main page
 close.addEventListener("click", function() {
-  hover.style.cursor = "pointer";
+  hover.style.display = "block";
+  close1.style.cursor = "pointer";
   tangerine.style.display = "none";
   sharpie.style.display = "none";
   ruler.style.display = "none";
