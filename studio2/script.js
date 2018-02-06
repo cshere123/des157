fbc38be810ff9430c8423a98fa3b8db739b085f8
seetho-c <cseetho@ucdavis.edu>
2018-02-05 16:54:10 -0800

    Update script.js

--- studio2/script.js
+++ studio2/script.js
@@ -1,44 +1,65 @@
-'use strict';
+"use strict";
 console.log("reading");
 // global variables
-var hover = document.getElementById('hover');
-var tangerine = document.getElementById('tangerine');
-var sharpie = document.getElementById('sharpie');
-var ruler = document.getElementById('ruler');
-var close = document.getElementById('close');
+var hover = document.getElementById("hover");
+var tangerine = document.getElementById("tangerine");
+var sharpie = document.getElementById("sharpie");
+var ruler = document.getElementById("ruler");
+var close = document.getElementById("close");
 
-var tangdescrip = document.getElementById('tangdescrip');
-var sharpiedescrip = document.getElementById('ssharpiedescrip');
-var rulerdescrip = document.getElementById('rulerdescrip');
+var tangdescrip = document.getElementById("tangdescrip");
+var sharpiedescrip = document.getElementById("ssharpiedescrip");
+var rulerdescrip = document.getElementById("rulerdescrip");
+var overlay = document.getElementById("overlay")
 
 //hover over bag to open
-hover.addEventListener('mouseout', function() {
+hover.addEventListener("mouseout", function() {
   hover.style.opacity = 1;
   hover.src = "images/backpack.png";
+  close.getElementById = "none";
 });
-hover.addEventListener('mouseover', function() {
+hover.addEventListener("mouseover", function() {
   hover.style.opacity = 1;
   hover.style.cursor = "pointer";
   hover.src = "images/hover.png";
+  close.getElementById = "none";
 });
 
 //after clicking on contents
-hover.addEventListener('click', function() {
-  tangerine.stye.display = 'block';
+hover.addEventListener("click", function() {
+  tangerine.stye.display = "block";
+  hover.style.opacity = 0;
+});
+
+tangerine.addEventListener("mouseover", function() {
   tangdescrip.style.display = "block";
+  hover.style.cursor = "pointer";
 });
 
-hover.addEventListener('click', function() {
-  sharpie.style.display = 'block';
-  sharpiedescrip.style.display = 'block';
+hover.addEventListener("click", function() {
+  sharpie.style.display = "block";
+  hover.style.opacity = 0;
+
 });
 
-hover.addEventListener('click', function() {
-  ruler.style.display = 'block';
-  rulerdescrip.style.display = 'block';
+sharpie.addEventListener("mouseover", function() {
+  sharpiedescrip.style.display = "block";
+  hover.style.cursor = "pointer";
 });
 
-tangerine.addEventListener('click', function() {
-  tang.style.cursor = 'pointer';
-  tangdescrip.style.display = 'block';
+hover.addEventListener("click", function() {
+  ruler.style.display = "block";
+  hover.style.opacity = 0;
+});
+
+ruler.addEventListener("mouseover", function() {
+  rulerdescrip.style.display = "block";
+  hover.style.cursor = "pointer";
+});
+
+close.addEventListener("click", function() {
+  hover.style.cursor = "pointer";
+  tangerine.style.display = "none";
+  sharpie.style.display = "none";
+  ruler.style.display = "none";
 });
