936f5ed2ccc4ba65477bfca453229ceefd0a439b
seetho-c <cseetho@ucdavis.edu>
2018-03-13 22:16:08 -0700

    Update share.js

--- project/proto3/share.js
+++ project/proto3/share.js
@@ -1,50 +1,16 @@
-//SCRIPT JS-------------------------------------//
-
-
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBU-4i6aqE_eNY3IosjA7ycNm33FXCF7eM",
-  authDomain: "unspoken-37e98.firebaseapp.com",
-  databaseURL: "https://unspoken-37e98.firebaseio.com",
-  projectId: "unspoken-37e98",
-  storageBucket: "",
-  messagingSenderId: "161766093627"
-};
-firebase.initializeApp(config);
-
-//reference messages collection
-var messagesRef = firebase.database().ref('messages');
-
-
-document.getElementById('shareform').addEventListener('submit', submitForm);
-
-//submit form
-function submitForm() {
-
-  //get values
-  var name = getInputVal('name');
-  var email = getInputVal('email');
-  var year = getInputVal('year');
-  var major = getInputVal('major');
-  var home = getInputVal('home');
-  var workschool = getInputVal('workschool');
-  var personal = getInputVal('personal');
-  var title = getInputVal('title');
-  var share = getInputVal('share');
-  var triggerwarning = getInputVal('triggerwarning');
-  var emailstory = getInputVal('emailstory');
-
-  //save message
-  saveMessage(name, email, year, major, home, workschool, personal, title, share, triggerwarning, emailstory);
-
-  //clear form after submission
-  document.getElementById('shareform').reset();
-}
+//FUNCTIONS--------------------------------------------------------------
 
 //function to get getform values
 function getInputVal(id) {
   return document.getElementById(id).value;
+}
 
+function getCheckedVal(id){
+  if (document.getElementById(id).checked==true){
+    return("on");
+  } else {
+    return("off");
+  }
 }
 
 // save message to firebase
@@ -64,8 +30,47 @@ function saveMessage(name, email, year, major, home, workschool, personal, title
     triggerwarning: triggerwarning,
     emailstory: emailstory,
   });
-  var overlay = document.getElementById("overlay");
-  if (onsubmit = "click") {
-    overlay.innerHTML = 'Thank you for your willingness and bravery to share your story with us';
-  };
+};
+
+
+// Initialize Firebase------------------------------------------------
+var config = {
+  apiKey: "AIzaSyBU-4i6aqE_eNY3IosjA7ycNm33FXCF7eM",
+  authDomain: "unspoken-37e98.firebaseapp.com",
+  databaseURL: "https://unspoken-37e98.firebaseio.com",
+  projectId: "unspoken-37e98",
+  storageBucket: "",
+  messagingSenderId: "161766093627"
+};
+firebase.initializeApp(config);
+
+//reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+//submit form
+function submitForm() {
+
+  //get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var year = getInputVal('year');
+  var major = getInputVal('major');
+  var home = getCheckedVal('home');
+  var workschool = getCheckedVal('workschool');
+  var personal = getCheckedVal('personal');
+  var title = getInputVal('title');
+  var share = getInputVal('share');
+  var triggerwarning = getInputVal('triggerwarning');
+  var emailstory = getCheckedVal('emailstory');
+
+  //save message
+  saveMessage(name, email, year, major, home, workschool, personal, title, share, triggerwarning, emailstory);
+
+  //clear form after submission
+  document.getElementById('shareform').reset();
+
+    // var overlaystories = document.getElementById("overlaystories");
+  // if (onsubmit = "click") {
+  //   alert('Thank you for your willingness and bravery to share your story with us');
+  // };
 };
