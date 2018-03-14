ea14f557b27ead6dc2a22c252e9e9fe71a86e7ec
seetho-c <cseetho@ucdavis.edu>
2018-03-13 22:09:15 -0700

    Update share.js

--- project/proto3/share.js
+++ project/proto3/share.js
@@ -19,15 +19,13 @@ var messagesRef = firebase.database().ref('messages');
 document.getElementById('shareform').addEventListener('submit', submitForm);
 
 //submit form
-function submitForm(e) {
-  e.preventDefault();
+function submitForm() {
 
   //get values
   var name = getInputVal('name');
   var email = getInputVal('email');
   var year = getInputVal('year');
   var major = getInputVal('major');
-  var healing = getInputVal('healing');
   var home = getInputVal('home');
   var workschool = getInputVal('workschool');
   var personal = getInputVal('personal');
@@ -37,10 +35,10 @@ function submitForm(e) {
   var emailstory = getInputVal('emailstory');
 
   //save message
-  saveMessage(name, email, year, major, healing, home, workschool, personal, title, share, triggerwarning, emailstory);
+  saveMessage(name, email, year, major, home, workschool, personal, title, share, triggerwarning, emailstory);
 
   //clear form after submission
-  document.getElementById('shareform').reset().loadNewPage('stories.html');
+  document.getElementById('shareform').reset();
 }
 
 //function to get getform values
@@ -50,7 +48,7 @@ function getInputVal(id) {
 }
 
 // save message to firebase
-function saveMessage(name, email, year, major, healing, home, workschool, personal, title, share, triggerwarning, emailstory) {
+function saveMessage(name, email, year, major, home, workschool, personal, title, share, triggerwarning, emailstory) {
   var newMessageRef = messagesRef.push();
   //sending an object of data to our messages collection in firebase
   newMessageRef.set({
@@ -58,7 +56,6 @@ function saveMessage(name, email, year, major, healing, home, workschool, person
     email: email,
     year: year,
     major: major,
-    healing: healing,
     home: home,
     workschool: workschool,
     personal: personal,
@@ -67,8 +64,8 @@ function saveMessage(name, email, year, major, healing, home, workschool, person
     triggerwarning: triggerwarning,
     emailstory: emailstory,
   });
-  var overlaystories = document.getElementById("overlaystories");
+  var overlay = document.getElementById("overlay");
   if (onsubmit = "click") {
-    alert('Thank you for your willingness and bravery to share your story with us');
+    overlay.innerHTML = 'Thank you for your willingness and bravery to share your story with us';
   };
 };
